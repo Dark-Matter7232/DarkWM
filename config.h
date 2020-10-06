66f93459549e24f7e0f00c2198dc8c000e961a33
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2020-10-06 23:43:00 +0530

    Add a startup chime

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const char *const autostart[] = {
 	"sh", "-c", "sxhkd -m 1 &", NULL,
 	"sh", "-c", "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)", NULL,
 	"sh", "-c", "numlockx &", NULL,
-	"sh", "-c", "pipewire &", NULL,
+	"sh", "-c", "mpv --volume=80 ~/.config/fixes/vistashutdown.ogv &", NULL, /*  add a starup chime cuz why not */
 	NULL /* terminate it */
 };
 
